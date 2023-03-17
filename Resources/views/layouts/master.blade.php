abc1bbc45cb6510d5a4dad29dae1f8fbf06d6e1f
Sorethea OK <sorethea@gmail.com>
2023-03-17 15:28:49 +0700

    first commit

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -8,12 +8,35 @@
 
        {{-- Laravel Vite - CSS File --}}
        {{-- {{ module_vite('build-market', 'Resources/assets/sass/app.scss') }} --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" >
+        <!-- style css for this template -->
+        {{ module_vite('build-market', 'Resources/assets/scss/style.css') }}
+{{--        <link href="assets/scss/style.css" rel="stylesheet" id="style">--}}
 
     </head>
     <body>
         @yield('content')
 
         {{-- Laravel Vite - JS File --}}
-        {{-- {{ module_vite('build-market', 'Resources/assets/js/app.js') }} --}}
+        {{ module_vite('build-market', 'Resources/assets/js/jquery-3.3.1.min.js') }}
+        {{ module_vite('build-market', 'Resources/assets/js/popper.min.js') }}
+        {{ module_vite('build-market', 'Resources/assets/vendor/bootstrap-5/js/bootstrap.bundle.min.js') }}
+
+        {{ module_vite('build-market', 'Resources/assets/js/main.js') }}
+        {{ module_vite('build-market', 'Resources/assets/js/color-scheme.js') }}
+
+        {{ module_vite('build-market', 'Resources/assets/js/app.js') }}
+
+        {{--        <!-- Required jquery and libraries -->--}}
+{{--        <script src="assets/js/jquery-3.3.1.min.js"></script>--}}
+{{--        <script src="assets/js/popper.min.js"></script>--}}
+{{--        <script src="assets/vendor/bootstrap-5/js/bootstrap.bundle.min.js"></script>--}}
+
+{{--        <!-- Customized jquery file  -->--}}
+{{--        <script src="assets/js/main.js"></script>--}}
+{{--        <script src="assets/js/color-scheme.js"></script>--}}
+
+{{--        <!-- page level custom script -->--}}
+{{--        <script src="assets/js/app.js"></script>--}}
     </body>
 </html>
