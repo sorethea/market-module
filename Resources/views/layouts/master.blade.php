954ae8d1adf8829d569ef311e0862c520fe04348
Sorethea OK <sorethea@gmail.com>
2023-03-17 16:16:04 +0700

    first commit

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -1,16 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Market</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="generator" content="">
+        <title>oneuiux V2.0 - Mobile HTML template</title>
+
+        <!-- manifest meta -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <link rel="manifest" href="{{asset('modules/market/manifest.json')}}" />
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" href="{{asset('img/favicon180.png')}}" sizes="180x180">
+        <link rel="icon" href="{{asset('img/favicon32.png')}}" sizes="32x32" type="image/png">
+        <link rel="icon" href="{{assets('img/favicon16.png')}}" sizes="16x16" type="image/png">
+
+        <!-- Google fonts-->
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+        <!-- bootstrap icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+        <!-- nouislider CSS -->
+        <link href="{{asset('vendor/nouislider/nouislider.min.css')}}" rel="stylesheet">
+
+        <!-- date rage picker -->
+        <link rel="stylesheet" href="{{asset('vendor/daterangepicker/daterangepicker.css')}}">
+
+        <!-- swiper carousel css -->
+        <link rel="stylesheet" href="{{asset('vendor/swiperjs-6.6.2/swiper-bundle.min.css')}}">
+
+        <!-- style css for this template -->
+        <link rel="stylesheet" href="{{ asset('modules/market/scss/style.css') }}">
 
        {{-- Laravel Vite - CSS File --}}
        {{-- {{ module_vite('build-market', 'Resources/assets/sass/app.scss') }} --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" >
         <!-- style css for this template -->
-        <link rel="stylesheet" href="{{ asset('modules/market/scss/style.css') }}">
+
 
 {{--        {{ module_vite('build-market', 'Resources/assets/scss/style.css') }}--}}
 {{--        <link href="assets/scss/style.css" rel="stylesheet" id="style">--}}
