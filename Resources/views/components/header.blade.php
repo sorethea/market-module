e430592855c70fb3761569666c58b30585469390
Sorethea OK <sorethea@gmail.com>
2023-03-23 14:59:30 +0700

    lam-dev

--- Resources/views/components/header.blade.php
+++ Resources/views/components/header.blade.php
@@ -6,21 +6,21 @@
                 <i class="bi bi-list"></i>
             </button>
         </div>
-        <div class="col">
-            <div class="logo-small">
-                <img src="assets/img/logo.png" alt="" class="rounded-circle" />
-                <h5>OneUIUX<br /><span class="text-secondary fw-light">Shopping</span></h5>
-            </div>
-        </div>
-        <div class="col-auto">
-            <a href="notifications.html" target="_self" class="btn btn-light btn-44 btn-rounded">
-                <i class="bi bi-bell"></i>
-                <span class="count-indicator"></span>
-            </a>
-            <a href="profile.html" target="_self" class="btn btn-light btn-44 btn-rounded ms-2">
-                <i class="bi bi-person-circle"></i>
-            </a>
-        </div>
+{{--        <div class="col">--}}
+{{--            <div class="logo-small">--}}
+{{--                <img src="assets/img/logo.png" alt="" class="rounded-circle" />--}}
+{{--                <h5>OneUIUX<br /><span class="text-secondary fw-light">Shopping</span></h5>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-auto">--}}
+{{--            <a href="notifications.html" target="_self" class="btn btn-light btn-44 btn-rounded">--}}
+{{--                <i class="bi bi-bell"></i>--}}
+{{--                <span class="count-indicator"></span>--}}
+{{--            </a>--}}
+{{--            <a href="profile.html" target="_self" class="btn btn-light btn-44 btn-rounded ms-2">--}}
+{{--                <i class="bi bi-person-circle"></i>--}}
+{{--            </a>--}}
+{{--        </div>--}}
     </div>
 </header>
 <!-- Header ends -->
