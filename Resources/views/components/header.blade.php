230d9a6d8fc6fee995e8de61cf3eefa582b300e6
Sorethea OK <sorethea@gmail.com>
2023-03-23 14:37:42 +0700

    lam-dev

---
+++ Resources/views/components/header.blade.php
@@ -0,0 +1,26 @@
+<!-- Header -->
+<header class="header position-fixed header-filled">
+    <div class="row">
+        <div class="col-auto">
+            <button type="button" class="btn btn-light btn-44 btn-rounded menu-btn">
+                <i class="bi bi-list"></i>
+            </button>
+        </div>
+        <div class="col">
+            <div class="logo-small">
+                <img src="assets/img/logo.png" alt="" class="rounded-circle" />
+                <h5>OneUIUX<br /><span class="text-secondary fw-light">Shopping</span></h5>
+            </div>
+        </div>
+        <div class="col-auto">
+            <a href="notifications.html" target="_self" class="btn btn-light btn-44 btn-rounded">
+                <i class="bi bi-bell"></i>
+                <span class="count-indicator"></span>
+            </a>
+            <a href="profile.html" target="_self" class="btn btn-light btn-44 btn-rounded ms-2">
+                <i class="bi bi-person-circle"></i>
+            </a>
+        </div>
+    </div>
+</header>
+<!-- Header ends -->
--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -47,9 +47,176 @@
     <!-- loader section ends -->
     <!-- menu section -->
         @include("market::components.menu")
+    <!-- Begin page -->
+    <main class="h-100">
+        @include("market::components.header")
     <!-- menu section ends -->
         @yield('content')
+    </main>
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            <ul class="nav nav-pills nav-justified">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.html">
+                        <span>
+                            <i class="nav-icon bi bi-house"></i>
+                            <span class="nav-text">Home</span>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="stats.html">
+                        <span>
+                            <i class="nav-icon bi bi-binoculars"></i>
+                            <span class="nav-text">Statistics</span>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item centerbutton">
+                    <a href="cart.html" class="nav-link" id="centermenubtn">
+                        <span class="theme-linear-gradient">
+                            <i class="bi bi-basket size-22"></i>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="myorders.html">
+                        <span>
+                            <i class="nav-icon bi bi-bag"></i>
+                            <span class="nav-text">Orders</span>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wallet.html">
+                        <span>
+                            <i class="nav-icon bi bi-wallet2"></i>
+                            <span class="nav-text">Wallet</span>
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+    <!-- Footer ends-->
 
+    <!-- filter menu -->
+    <div class="filter">
+        <div class="card shadow h-100">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col align-self-center">
+                        <h6 class="mb-0">Filter Criteria</h6>
+                        <p class="text-secondary small">2154 products</p>
+                    </div>
+                    <div class="col-auto px-0">
+                        <button class="btn btn-link text-danger filter-close">
+                            <i class="bi bi-x size-22"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body overflow-auto">
+                <div class="mb-4">
+                    <h6>Select Range</h6>
+                    <div id="rangeslider" class="mt-4"></div>
+                </div>
+
+                <div class="row mb-4">
+                    <div class="col">
+                        <div class="form-floating">
+                            <input type="number" class="form-control" min="0" max="500" value="100" step="1" id="input-select">
+                            <label for="input-select">Minimum</label>
+                        </div>
+                    </div>
+                    <div class="col-auto align-self-center"> to </div>
+                    <div class="col">
+                        <div class="form-floating">
+                            <input type="number" class="form-control" min="0" max="500" value="200" step="1" id="input-number">
+                            <label for="input-number">Maximum</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-floating mb-4">
+                    <select class="form-control" id="filtertype">
+                        <option selected>Cloths</option>
+                        <option>Electronics</option>
+                        <option>Furniture</option>
+                    </select>
+                    <label for="filtertype">Select Shopping Type</label>
+                </div>
+
+                <div class="form-group floating-form-group active mb-4">
+                    <h6 class="mb-3">Select Facilities</h6>
+
+                    <div class="form-check form-switch mb-2">
+                        <input type="checkbox" class="form-check-input" id="men" checked>
+                        <label class="form-check-label" for="men">Men</label>
+                    </div>
+                    <div class="form-check form-switch mb-2">
+                        <input type="checkbox" class="form-check-input" id="woman" checked>
+                        <label class="form-check-label" for="woman">Women</label>
+                    </div>
+                    <div class="form-check form-switch mb-2">
+                        <input type="checkbox" class="form-check-input" id="Sport">
+                        <label class="form-check-label" for="Sport">Sport</label>
+                    </div>
+                    <div class="form-check form-switch mb-2">
+                        <input type="checkbox" class="form-check-input" id="homedecor">
+                        <label class="form-check-label" for="homedecor">Home Decor</label>
+                    </div>
+                    <div class="form-check form-switch mb-2">
+                        <input type="checkbox" class="form-check-input" id="kidsplay">
+                        <label class="form-check-label" for="kidsplay">Kid's Play</label>
+                    </div>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" placeholder="Keyword">
+                    <label for="input-select">Keyword</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <select class="form-control" id="discount">
+                        <option>10% </option>
+                        <option>30%</option>
+                        <option>50%</option>
+                        <option>80%</option>
+                    </select>
+                    <label for="discount">Offer Discount</label>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-default w-100 shadow-sm shadow-success btn-rounded">Search</button>
+            </div>
+        </div>
+    </div>
+    <!-- filter menu ends-->
+
+    <!-- PWA app install toast message -->
+    <div class="position-fixed bottom-0 start-50 translate-middle-x  z-index-99">
+        <div class="toast mb-3" role="alert" aria-live="assertive" aria-atomic="true" id="toastinstall" data-bs-animation="true">
+            <div class="toast-header">
+                <img src="assets/img/favicon32.png" class="rounded me-2" alt="...">
+                <strong class="me-auto">Install PWA App</strong>
+                <small>now</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                <div class="row">
+                    <div class="col">
+                        Click "Install" to install PWA app & experience indepedent.
+                    </div>
+                    <div class="col-auto align-self-center ps-0">
+                        <button class="btn-default btn btn-sm btn-rounded" id="addtohome">Install</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page ends-->
         <script src="{{ asset('modules/market/js/jquery-3.3.1.min.js') }}"></script>
         <script src="{{ asset('modules/market/js/popper.min.js') }}"></script>
         <script src="{{ asset('modules/market/js/vendor/bootstrap-5/js/bootstrap.bundle.min.js') }}"></script>
