8a7c9288504a7b6acee00ab2bd954a1049e8bd8b
Sorethea OK <sorethea@gmail.com>
2023-03-23 10:53:33 +0700

    lam-dev

--- Providers/InstallServiceProvider.php
+++ Providers/InstallServiceProvider.php
@@ -4,12 +4,21 @@ namespace Modules\Market\Providers;
 
 use Modules\Market\Database\Seeders\MarketDatabaseSeeder;
 use Modules\LAM\Providers\BaseInstallServiceProvider;
+use Modules\Market\Database\Settings\CreateMarketSettings;
 
 class InstallServiceProvider extends BaseInstallServiceProvider
 {
+    private array $settings =[
+        CreateMarketSettings::class,
+    ];
     public function install()
     {
         $seed = new MarketDatabaseSeeder();
         $seed?->run();
+
+        foreach ($this->settings as $setting){
+            $settingObj = new $setting();
+            $settingObj?->up();
+        }
     }
 }
--- Providers/UninstallServiceProvider.php
+++ Providers/UninstallServiceProvider.php
@@ -4,12 +4,21 @@ namespace Modules\Market\Providers;
 
 use Modules\Market\Database\Seeders\MarketDatabaseSeeder;
 use Modules\LAM\Providers\BaseUninstallServiceProvider;
+use Modules\Market\Database\Settings\CreateMarketSettings;
 
 class UninstallServiceProvider extends BaseUninstallServiceProvider
 {
+    private array $settings =[
+        CreateMarketSettings::class,
+    ];
     public function uninstall()
     {
         $seed = new MarketDatabaseSeeder();
         $seed->rollback();
+
+        foreach ($this->settings as $setting){
+            $settingObj = new $setting();
+            $settingObj?->down();
+        }
     }
 }
