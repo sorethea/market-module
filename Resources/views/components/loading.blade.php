6f71150f0a018aa0bd83db81023c784e0ac8045f
Sorethea OK <sorethea@gmail.com>
2023-03-23 09:55:51 +0700

    lam-dev

---
+++ Resources/views/components/loading.blade.php
@@ -0,0 +1,18 @@
+<div class="container-fluid loader-wrap">
+    <div class="row h-100">
+        <div class="col-10 col-md-6 col-lg-5 col-xl-3 mx-auto text-center align-self-center">
+            <div class="circular-loader">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <p class="mt-4"><span class="text-secondary">HiEAT, experience with the best delivery services.</span><br><strong>Please
+                    wait...</strong></p>
+        </div>
+    </div>
+</div>
--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -43,24 +43,7 @@
     </head>
     <body class="body-scroll theme-red">
     <!-- loader section -->
-    <div class="container-fluid loader-wrap">
-        <div class="row h-100">
-            <div class="col-10 col-md-6 col-lg-5 col-xl-3 mx-auto text-center align-self-center">
-                <div class="circular-loader">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <p class="mt-4"><span class="text-secondary">HiEAT, experience with the best delivery services in Cambodia</span><br><strong>Please
-                        wait...</strong></p>
-            </div>
-        </div>
-    </div>
+        @include("market::components.loading")
     <!-- loader section ends -->
         @yield('content')
 
