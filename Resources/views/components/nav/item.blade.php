ef63633334592799dd0cde78eddee94e0720baaf
Sorethea OK <sorethea@gmail.com>
2023-04-04 10:57:27 +0700

    lam-dev

--- Resources/views/components/nav/item.blade.php
+++ Resources/views/components/nav/item.blade.php
@@ -1,9 +1,7 @@
-<div>
-    <li class="nav-item">
-        <a class="nav-link" href="{{$url}}" tabindex="-1">
-            <div class="avatar avatar-40 icon"><i class="bi bi-{{$icon}}"></i></div>
-            <div class="col">{{$name}}</div>
-            <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-        </a>
-    </li>
-</div>
+<li class="nav-item">
+    <a class="nav-link" href="{{$url}}" tabindex="-1">
+        <div class="avatar avatar-40 icon"><i class="bi bi-{{$icon}}"></i></div>
+        <div class="col">{{$name}}</div>
+        <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+    </a>
+</li>
