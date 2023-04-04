d855129b50b6c2bf417e5c3cc86dd6e74959bf02
Sorethea OK <sorethea@gmail.com>
2023-04-04 11:20:57 +0700

    lam-dev

--- Resources/views/components/nav/dropdown.blade.php
+++ Resources/views/components/nav/dropdown.blade.php
@@ -1,6 +1,58 @@
-<div>
-    <!-- 
-  <options=bold>“ Very little is needed to make a happy life. ”</>
-  <fg=gray>— Marcus Aurelius</>
- -->
-</div>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+        <div class="avatar avatar-40 icon"><i class="bi bi-cart"></i></div>
+        <div class="col">Shop pages</div>
+        <div class="arrow"><i class="bi bi-chevron-down plus"></i> <i class="bi bi-chevron-up minus"></i>
+        </div>
+    </a>
+    <ul class="dropdown-menu">
+        <li>
+            <a class="dropdown-item nav-link" href="products.html">
+                <div class="avatar avatar-40 icon"><i class="bi bi-box-seam"></i>
+                </div>
+                <div class="col align-self-center">All Products</div>
+                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item nav-link" href="product.html">
+                <div class="avatar avatar-40 icon"><i class="bi bi-box-seam"></i>
+                </div>
+                <div class="col align-self-center">Product</div>
+                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item nav-link" href="cart.html">
+                <div class="avatar avatar-40 icon"><i class="bi bi-bag"></i>
+                </div>
+                <div class="col align-self-center">Cart</div>
+                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item nav-link" href="myorders.html">
+                <div class="avatar avatar-40 icon"><i class="bi bi-view-list"></i>
+                </div>
+                <div class="col align-self-center">My Orders</div>
+                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item nav-link" href="payment.html">
+                <div class="avatar avatar-40 icon"><i class="bi bi-cash-stack"></i>
+                </div>
+                <div class="col align-self-center">Payment</div>
+                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item nav-link" href="invoice.html">
+                <div class="avatar avatar-40 icon"><i class="bi bi-receipt"></i>
+                </div>
+                <div class="col align-self-center">Invoice</div>
+                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
+            </a>
+        </li>
+    </ul>
+</li>
--- Resources/views/components/sidebar.blade.php
+++ Resources/views/components/sidebar.blade.php
@@ -7,64 +7,7 @@
             <ul class="nav nav-pills">
                 <x-market-nav-item url="market/message" name="Message" icon="chat-text"/>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
-                        <div class="avatar avatar-40 icon"><i class="bi bi-cart"></i></div>
-                        <div class="col">Shop pages</div>
-                        <div class="arrow"><i class="bi bi-chevron-down plus"></i> <i class="bi bi-chevron-up minus"></i>
-                        </div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a class="dropdown-item nav-link" href="products.html">
-                                <div class="avatar avatar-40 icon"><i class="bi bi-box-seam"></i>
-                                </div>
-                                <div class="col align-self-center">All Products</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item nav-link" href="product.html">
-                                <div class="avatar avatar-40 icon"><i class="bi bi-box-seam"></i>
-                                </div>
-                                <div class="col align-self-center">Product</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item nav-link" href="cart.html">
-                                <div class="avatar avatar-40 icon"><i class="bi bi-bag"></i>
-                                </div>
-                                <div class="col align-self-center">Cart</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item nav-link" href="myorders.html">
-                                <div class="avatar avatar-40 icon"><i class="bi bi-view-list"></i>
-                                </div>
-                                <div class="col align-self-center">My Orders</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item nav-link" href="payment.html">
-                                <div class="avatar avatar-40 icon"><i class="bi bi-cash-stack"></i>
-                                </div>
-                                <div class="col align-self-center">Payment</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item nav-link" href="invoice.html">
-                                <div class="avatar avatar-40 icon"><i class="bi bi-receipt"></i>
-                                </div>
-                                <div class="col align-self-center">Invoice</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+                <x-market-nav-dropdown />
                 <li class="nav-item">
                     <a class="nav-link" href="chat.html" tabindex="-1">
                         <div class="avatar avatar-40 icon"><i class="bi bi-chat-text"></i></div>
