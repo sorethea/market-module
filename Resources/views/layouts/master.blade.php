3f0dfc5048d5ca8b72bc83642338f02a3bba647b
Sorethea OK <sorethea@gmail.com>
2023-04-04 13:30:47 +0700

    lam-dev

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -53,51 +53,7 @@
         @yield('content')
     </main>
     <!-- Footer -->
-    <footer class="footer">
-        <div class="container">
-            <ul class="nav nav-pills nav-justified">
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.html">
-                        <span>
-                            <i class="nav-icon bi bi-house"></i>
-                            <span class="nav-text">Home</span>
-                        </span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="stats.html">
-                        <span>
-                            <i class="nav-icon bi bi-binoculars"></i>
-                            <span class="nav-text">Statistics</span>
-                        </span>
-                    </a>
-                </li>
-                <li class="nav-item centerbutton">
-                    <a href="cart.html" class="nav-link" id="centermenubtn">
-                        <span class="theme-linear-gradient">
-                            <i class="bi bi-basket size-22"></i>
-                        </span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="myorders.html">
-                        <span>
-                            <i class="nav-icon bi bi-bag"></i>
-                            <span class="nav-text">Orders</span>
-                        </span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="wallet.html">
-                        <span>
-                            <i class="nav-icon bi bi-wallet2"></i>
-                            <span class="nav-text">Wallet</span>
-                        </span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </footer>
+
     <!-- Footer ends-->
 
     <!-- filter menu -->
